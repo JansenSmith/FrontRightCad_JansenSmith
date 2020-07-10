768a7c72c83cd86698e4de204386989b68ad6b6d
Xavier <Xavier@10.71.5.179>
2020-07-10 02:05:33 -0400

    Added Leg STLs to CAD

--- FrontRightCad.groovy
+++ FrontRightCad.groovy
@@ -36,39 +36,120 @@ return new ICadGenerator(){
 		ArrayList<CSG> allCad=new ArrayList<>();
 		File legFile = null
 		if(linkIndex ==0){
-			legFile = ScriptingEngine.fileFromGit(
-			"https://github.com/OperationSmallKat/Luna.git",
-			"LunaShoulderMirror.stl");
-			def body = Vitamins.get(legFile);
-			allCad.add(body);
+			CSG shoulder  = Vitamins.get(ScriptingEngine.fileFromGit(
+				"https://github.com/OperationSmallKat/Luna.git",
+				"Shoulder Base.stl"))
+				.rotz(180)
+				.rotx(180)
+				.setColor(javafx.scene.paint.Color.DARKGREY)
+			CSG shouldertop  = Vitamins.get(ScriptingEngine.fileFromGit(
+				"https://github.com/OperationSmallKat/Luna.git",
+				"Servo Cover 2.stl"))
+				.rotz(180)
+				.rotx(180)
+				.setColor(javafx.scene.paint.Color.DARKGREY)
+			CSG dlimb  = Vitamins.get(ScriptingEngine.fileFromGit(
+				"https://github.com/OperationSmallKat/Luna.git",
+				"Drive Limb.stl"))
+				.rotz(180)
+				.rotx(90)
+				.movex(-44.5)
+				.movey(23.56)
+				.setColor(javafx.scene.paint.Color.YELLOW)
+			CSG elimb  = Vitamins.get(ScriptingEngine.fileFromGit(
+				"https://github.com/OperationSmallKat/Luna.git",
+				"Encoder Limb.stl"))
+				.rotz(180)
+				.rotx(-90)
+				.movex(-44.5)
+				.movey(-9.94)
+				.setColor(javafx.scene.paint.Color.YELLOW)
+			allCad.add(shoulder);
+			allCad.add(shouldertop);
+			allCad.add(dlimb);
+			allCad.add(elimb);
 
 		}
 		if(linkIndex ==1){
-			legFile = ScriptingEngine.fileFromGit(
-			"https://github.com/OperationSmallKat/Luna.git",
-			"LunaKneeMirror.stl");
-			def body = Vitamins.get(legFile);
-			allCad.add(body);
+			CSG knee  = Vitamins.get(ScriptingEngine.fileFromGit(
+				"https://github.com/OperationSmallKat/Luna.git",
+				"Knee Base.stl"))
+				.rotz(180)
+				.setColor(javafx.scene.paint.Color.DARKGREY)
+			CSG kneetop  = Vitamins.get(ScriptingEngine.fileFromGit(
+				"https://github.com/OperationSmallKat/Luna.git",
+				"Knee Top.stl"))
+				.rotz(180)
+				.setColor(javafx.scene.paint.Color.DARKGREY)
+			CSG dlimb  = Vitamins.get(ScriptingEngine.fileFromGit(
+				"https://github.com/OperationSmallKat/Luna.git",
+				"Drive Limb.stl"))
+				.rotz(180)
+				.movex(-55.5)
+				.movez(16.5)
+				.setColor(javafx.scene.paint.Color.YELLOW)
+			CSG elimb  = Vitamins.get(ScriptingEngine.fileFromGit(
+				"https://github.com/OperationSmallKat/Luna.git",
+				"Encoder Limb.stl"))
+				.rotz(180)
+				.rotx(180)
+				.movex(-55.5)
+				.movez(-16.5)
+				.setColor(javafx.scene.paint.Color.YELLOW)
+			allCad.add(knee);
+			allCad.add(kneetop);
+			allCad.add(dlimb);
+			allCad.add(elimb);
 
 		}
 
 		if(linkIndex ==2){
-			legFile = ScriptingEngine.fileFromGit(
-			"https://github.com/OperationSmallKat/Luna.git",
-			"LunaFootMirror.stl");
-			def body = Vitamins.get(legFile);
-			allCad.add(body);
+			CSG foot  = Vitamins.get(ScriptingEngine.fileFromGit(
+				"https://github.com/OperationSmallKat/Luna.git",
+				"Foot.stl"))
+				.rotz(180)
+				.rotx(180)
+				.setColor(javafx.scene.paint.Color.DARKGREY)
+			CSG dlimb  = Vitamins.get(ScriptingEngine.fileFromGit(
+				"https://github.com/OperationSmallKat/Luna.git",
+				"Drive Limb.stl"))
+				.rotz(180-7.5)
+				.movex(-60)
+				.movez(16.5)
+				.setColor(javafx.scene.paint.Color.YELLOW)
+			CSG elimb  = Vitamins.get(ScriptingEngine.fileFromGit(
+				"https://github.com/OperationSmallKat/Luna.git",
+				"Encoder Limb.stl"))
+				.rotz(180+7.5)
+				.rotx(180)
+				.movex(-60)
+				.movez(-16.5)
+				.setColor(javafx.scene.paint.Color.YELLOW)
+			allCad.add(foot);
+			allCad.add(dlimb);
+			allCad.add(elimb);
 		}
 		
 		ArrayList<DHLink> dhLinks = d.getChain().getLinks()
 		DHLink dh = dhLinks.get(linkIndex)
+		// Hardware to engineering units configuration
+		LinkConfiguration conf = d.getLinkConfiguration(linkIndex);
+		// Engineering units to kinematics link (limits and hardware type abstraction)
+		AbstractLink abstractLink = d.getAbstractLink(linkIndex);// Transform used by the UI to render the location of the object
+		// Transform used by the UI to render the location of the object
 		Affine manipulator = dh.getListener();
 		
-		for(def body: allCad)
+		for(CSG body: allCad)
 		body.setManipulator(manipulator);
 		
 		return allCad;
 		
+	}
+
+	@Override
+	public ArrayList<CSG> generateBody(MobileBase arg0) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 };
